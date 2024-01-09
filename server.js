0f4a196a1b2957e7968b60be8948de36402e190b
Jainam Desai <jainam.d@kalvium.com>
2024-01-09 15:43:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,8 +4,10 @@ const port = process.env.PUBLIC_PORT;
 
 // define the ping route
 
-app.listen(port, () => {
-  console.log(`🚀 server running on PORT: ${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`🚀 server running on PORT: ${port}`);
+  });
+}
 
 module.exports = app;
