a7fd4c3dc3bcb1b3b645407c2ccf67e34de86988
Jainam Desai <jainam.d@kalvium.com>
2024-01-15 13:01:12 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const port = process.env.PUBLIC_PORT;
+
+// define the ping route
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`🚀 server running on PORT: ${port}`);
+  });
+}
+
+module.exports = app;
