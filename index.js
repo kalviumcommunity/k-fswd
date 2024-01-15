3855cbd5cde54fa2bf73d12d0a35d13d2ca7ace1
Jainam Desai <jainam.d@kalvium.com>
2024-01-15 15:30:49 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+// import the require module
+
+// read from the pocess.env and assign the values
+const config = {
+  api_key: '',
+  secrect: '',
+  is_kalvian: process.env.IS_KALVIAN ?? false
+}
+
+// assign the config to module.exports
+module.exports = {}
