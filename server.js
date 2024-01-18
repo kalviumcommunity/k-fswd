d64268421199394437dc14c072a0af84e7332b07
siddhardha123 <siddhardha123123@gmail.com>
2024-01-18 10:06:23 +0530

    Add plu2 for module 8

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');
+const app = express();
+
+app.use(express.json());
+
+// use the endpoint below and hit the end point with the following data:
+// { "best_ug_degree": "kalvium" }
+app.post('/', (req, res) => {
+    const data = req.body;
+
+    fs.writeFile('data.js', JSON.stringify(data, null, 2), (err) => {
+        if (err) {
+            console.error(err);
+            res.status(500).send('Error writing to file');
+        } else {
+            res.status(200).send('Data written to file');
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+module.exports = app;
