2b0070411c4484f4ac7a4ca5df1dbc974b3b12e2
th3c0d3br34ker <jainamdesai.learn@gmail.com>
2024-01-19 08:35:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ const app = express();
 const port = process.env.PUBLIC_PORT ?? 8000;
 
 const users = genrateData();
+// console.log(users);
 
 // define the /users route with the response in JSON
 // if the process.env.IS_ADMIN is true, then return the personal property as well
 app.get('/users', (req, res) => {
-  // Your code here
+  const { IS_ADMIN } = process.env;
+
+  if (IS_ADMIN === 'true') {
+    return res.json(users);
+  }
+
+  return res.json(users.map(({ personal, ...user }) => user));
 });
 
 if (require.main === module) {
