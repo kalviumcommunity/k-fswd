2ff487de86b89dcebc886a7ec9c25dc77b0beb80
th3c0d3br34ker <jainamdesai.learn@gmail.com>
2024-01-24 12:46:51 +0530

    Update boilerplate code

--- server.js
+++ server.js
@@ -9,10 +9,12 @@ app.use(express.json());
 // user.json file is defined in the same directory as server.js
 // Create a suitable endpoint to update only name in the user.json file
 // After creating the endpoint make the request from bruno
+// Endpoint: /update-user
 
 // Task 2
 // Create a suitable endpoint to replace the entire user object in user.json file
 // After creating the endpoint make the request from bruno
+// Endpoint: /replace-user
 
 
 if (require.main === module) {
