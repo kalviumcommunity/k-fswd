508e25d950e719cd18052ccc31f1afbcc25b584a
Vaibhav Pathak <81844384+vaibhav-zemo@users.noreply.github.com>
2024-01-25 14:37:19 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,12 +1,2 @@
 // read from the 'dev' folder
 // write to the 'build' folder
-
-
-const fs = require('fs');
-
-const files = fs.readdirSync('./dev');
-
-
-files.forEach(file => {
-    fs.copyFileSync(`./dev/${file}`, `./build/${file}`);
-})
