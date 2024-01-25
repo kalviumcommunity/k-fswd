3cfb6b3055dbb04e48c710e371edb5668570b188
Vaibhav Pathak <81844384+vaibhav-zemo@users.noreply.github.com>
2024-01-25 15:32:47 +0530

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,2 +1,22 @@
 // read from the 'dev' folder
 // write to the 'build' folder
+const fs = require('fs');
+const path = require('path');
+
+// Function to read index.html from dev folder
+function readFromDev() {
+  
+}
+
+
+// Function to write index.html to build folder
+function writeToBuild() {
+  
+}
+
+writeToBuild()
+
+module.exports = {
+  readFromDev,
+  writeToBuild,
+};
