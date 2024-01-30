af79527aa80bc2612b60f72f98c18b2e0e99e27c
Vaibhav Pathak <81844384+vaibhav-zemo@users.noreply.github.com>
2024-01-30 15:13:57 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+function validateUserInput(input){
+    // your code here
+}
+
+const input = {
+ 	 "name": "<your name>",
+ 	 "squad": "<your squad>",
+ 	 "dob": "<your dob>",
+ 	 "github": "<your github>"
+ }
+
+console.log(validateUserInput(input))
