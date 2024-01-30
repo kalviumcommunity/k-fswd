3b2668b33264fc15878aab5ac04aade1d6d07853
Vaibhav Pathak <81844384+vaibhav-zemo@users.noreply.github.com>
2024-01-30 14:33:35 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PUBLIC_PORT ?? 8000;
+
+app.use(express.json());
+
+// 1. define a POST endpoint
+// 2. use the below json object as request body to test the route from bruno
+
+app.post('/api/data', (req, res) => {
+  // your code here
+  
+  res.json({}); // replace this with your response
+});
+
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`🚀 server running on PORT: ${port}`);
+  });
+}
+
+module.exports = app;
