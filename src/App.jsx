4062ee382d7355f8c2d5bfa2955e29f90b569ebb
Vaibhav Pathak <81844384+vaibhav-zemo@users.noreply.github.com>
2024-01-31 16:47:40 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import { useEffect, useState } from 'react';
 function App() {
   const [data, setData] = useState([]);
 
-  // Fetch the data from the API
-	// The API is available at `https://jsonplaceholder.typicode.com/posts`
-
   return (
     <>
       <pre>{JSON.stringify(data, null, 2)}</pre>
