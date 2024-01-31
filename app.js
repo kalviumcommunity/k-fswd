7382f6e954abd476b092fc4a913a6569cecff94e
th3c0d3br34ker <jainamdesai.learn@gmail.com>
2024-01-31 16:25:48 +0530

    Updated boilerplate code

---
+++ app.js
@@ -0,0 +1,98 @@
+const MIN = 100;
+const MAX = 999;
+const pinInput = document.getElementById('pin');
+const sha256HashView = document.getElementById('sha256-hash');
+const saltView = document.getElementById('sha256-salt');
+const resultView = document.getElementById('result');
+const SALT = 'happy'
+
+// a function to store in the local storage
+function store(key, value) {
+  localStorage.setItem(key, value);
+}
+
+// a function to retrieve from the local storage
+function retrieve(key) {
+  return localStorage.getItem(key);
+}
+
+function getRandomArbitrary(min, max) {
+  let cached;
+  cached = Math.random() * (max - min) + min;
+  cached = Math.floor(cached);
+  return cached;
+}
+
+// a function to clear the local storage
+function clear() {
+  localStorage.clear();
+}
+
+// a function to generate sha256 hash of the given string along the salt
+async function sha256(message, salt) {
+  const saltedMessage = `${salt}${message}`;
+  // encode as UTF-8
+  const msgBuffer = new TextEncoder().encode(saltedMessage);
+
+  // hash the message
+  const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
+
+  // convert ArrayBuffer to Array
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+
+  // convert bytes to hex string
+  const hashHex = hashArray
+    .map((b) => b.toString(16).padStart(2, '0'))
+    .join('');
+  return hashHex;
+}
+
+async function getSHA256Hash() {
+  let cached = retrieve('sha256');
+  if (cached) {
+    return cached;
+  }
+
+  cached = await sha256(getRandomArbitrary(MIN, MAX), SALT);
+  store('sha256', cached);
+  return cached;
+}
+
+async function main() {
+  saltView.innerHTML = `Use the salt: '${SALT}'`;
+  sha256HashView.innerHTML = 'Calculating...';
+  const hash = await getSHA256Hash();
+  sha256HashView.innerHTML = hash;
+}
+
+async function test() {
+  const pin = pinInput.value;
+
+  if (pin.length !== 3) {
+    resultView.innerHTML = '💡 not 3 digits';
+    resultView.classList.remove('hidden');
+    return;
+  }
+
+  const sha256HashView = document.getElementById('sha256-hash');
+  const hasedPin = await sha256(pin);
+
+  if (hasedPin === sha256HashView.innerHTML) {
+    resultView.innerHTML = '🎉 success';
+    resultView.classList.add('success');
+  } else {
+    resultView.innerHTML = '❌ failed';
+  }
+  resultView.classList.remove('hidden');
+}
+
+// ensure pinInput only accepts numbers and is 3 digits long
+pinInput.addEventListener('input', (e) => {
+  const { value } = e.target;
+  pinInput.value = value.replace(/\D/g, '').slice(0, 3);
+});
+
+// attach the test function to the button
+document.getElementById('check').addEventListener('click', test);
+
+main();
