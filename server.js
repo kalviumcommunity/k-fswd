f42cc0bbb5a3d27a66b580ed31fa1b6456acd545
Vaibhav Pathak <81844384+vaibhav-zemo@users.noreply.github.com>
2024-02-01 11:56:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const payload = { best_ug_degree: "kalvium" };
 
 const token = encrypt(payload);
-if (payload == decrypt(token).best_ug_degree) {
+if (payload == decrypt(token)?.best_ug_degree) {
     console.log("Success");
 }
 else {
