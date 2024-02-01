1574829a5c0d3a86f9763a12ae1ddb41887251ac
Vaibhav Pathak <vaibhavpathak852947@gmail.com>
2024-02-01 14:51:44 +0530

    add boilerplate code

---
+++ index.js
@@ -0,0 +1,18 @@
+const SECRET = 'Kalvium'
+const privateKey = "-----BEGIN RSA PRIVATE KEY-----MIICWwIBAAKBgQCJnrbfuAVy1NMATEGha8+Qamv3P2gnStO9m3Ysv6C3J0K33oIof4ZBmL5JVY2/xA+fY6JjLGmjXEg5saLSCwiDa+/wT0yOBvBem/9ktILg07BvKMkP/V6ezJdkKkkGxVQzMg5IRdRuBQY/jedhbvNbJS7lXJdHeqMh4nSqHrLJnQIDAQABAoGAY9yrzwN10LHmutya80uMPrs6xRtZtMlzhK7+vdIACWeP3mS0BjcjcQ4pZoWSaokW3Q4rk9NyhHw+XL8t60TeU4zko26SMfvEejScIbhiMK/nDvpZeN5jWaSW4LlioJTCXfKkJEnD7p6WJ9AnazQKqouQ2NSd9ZHYLi2Jp2aYiPECQQDT1ocbserz2010XS1NF/rJSyATHgQzrZC5CExcNAo2Y/Ewa8XQ+Kk6kC4mQR+hBGxlvnoppuMZyripqKaPMjYTAkEApk9IlYZvwINumZ062K3WYA2teb+zLq2Lhm6daa3r37vw2E17xWkQHpsn1E5+duOMrT9YEfqxxGb6RvHXauW3jwJAcwNgq5q3k9qXfPwx4k+Iq82MBpUfdudfUfa9broaaVlt0/pPlpvLVa5+P+rPv15ldAAs13xyhMhZ5lNYyxpZ3QJAaCudEVuFwBYTPtEHPRWWA9BNascymvhFPs0meD1iU1/LOhE+h6aJXEJfLlKEUD+zrAjTR1+53YWLGsj8345H7QJAPj6SwzdobHHIeCR1iwOvS5RWv3CqqEOASuad7QKlxs/MT4kC0al9ncX+ppB8eGQ5ZPQZYB+jJPIw7iRvkMk7xQ==-----END RSA PRIVATE KEY-----"
+
+const encryptHS256 = (payload) => {
+    // your code here
+
+
+    return token;
+}
+
+const encryptRS256 = (payload) => {
+    // your code here
+
+
+    return token;
+}
+
+module.exports = { encryptHS256, encryptRS256 };
