d6c47cd5533fe8539676a573359d45f7339c0cbf
Vaibhav Pathak <vaibhavpathak852947@gmail.com>
2024-02-06 15:33:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,12 +7,16 @@ const NodeCache = require('node-cache');
 const cache = new NodeCache({ stdTTL: 100, checkperiod: 120 });
 
 app.get('/api/doors', (req, res) => {
-    const data = cache.get('doors');
-    if (data) {
-        return res.json(data);
-    }
-    else {
-        cache.set('doors', db);
+    try {
+        let data = cache.get('doors');
+        if (!data) {
+            data = db;
+            cache.set('doors', data);
+        }
+        
+        return res.status(200).json({ data });
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
     }
 })
 
