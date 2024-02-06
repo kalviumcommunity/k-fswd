9f3b0b52c58155ebc74ecc3c7358347a1a285ad3
th3c0d3br34ker <jainamdesai.learn@gmail.com>
2024-02-06 14:23:55 +0530

    Update boilerplate code

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import UpdateItem from "./components/UpdateItem";
 
 // use the following link to get the data
+// `/doors` will give you all the doors, to get a specific door use `/doors/1`.
 const API_URI = `https://${import.meta.env.VITE_API_URI}/doors`;
 
 function App() {
