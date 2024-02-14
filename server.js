aa15e896ee366d5b4c9fb446d3f1a5fa9dfff831
Jainam Desai <jainam.d@kalvium.com>
2024-02-14 16:14:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,23 @@ const port = process.env.PUBLIC_PORT ?? 8000;
 app.use(express.json());
 
 // 1. define a POST endpoint
-// 2. use the below json object as request body to test the route from bruno
+// 2. use the below json objects as request body to test the route from bruno
+
+// admin JSON
+// {
+//   "username": "ram",
+//   "email": "ram@example.com",
+//   "role": "admin",
+//   "status": "active"
+// }
+
+// user JSON
+// {
+//   "username": "raj",
+//   "email": "raj@example.com",
+//   "role": "user",
+//   "is_banned": false
+// }
 
 app.post('/api/data', (req, res) => {
   // your code here
